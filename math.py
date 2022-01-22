035b29e882dbd5fb5a92127e94d4dcca890dea2f
Swaraj1120 <swarajofficial94@gmail.com>
2022-01-22 13:37:34 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,20 @@
+# Add implementation
+def add(x,y):
+	return x+y
+
+# Substract implementation
+def substract(x,y):
+	return x-y               # On master branch
+
+# Multiply implementation
+def multiply(x,y):
+	return x*y             # on Bug456 branch
+
+# Divide implementation function
+def divide(x,y):                                  #on master branch
+	if y==0:
+		return "divide by zero"
+        else :
+		return x/y
+
+
