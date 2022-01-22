b38d27dd3f2bd406f1be5020f04b2d48943ba935
Swaraj1120 <swarajofficial94@gmail.com>
2022-01-22 13:44:18 +0530

    added pass in place of code

--- math.py
+++ math.py
@@ -1,20 +1,17 @@
 # Add implementation
 def add(x,y):
-	return x+y
+	pass
 
 # Substract implementation
 def substract(x,y):
-	return x-y               # On master branch
+	pass               # On master branch
 
 # Multiply implementation
 def multiply(x,y):
-	return x*y             # on Bug456 branch
+	pass            # on Bug456 branch
 
 # Divide implementation function
 def divide(x,y):                                  #on master branch
-	if y==0:
-		return "divide by zero"
-        else :
-		return x/y
+	pass
 
 
